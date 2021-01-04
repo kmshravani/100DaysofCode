c15c54603933220ac309a1d578eebf3195adc21b
Shravani Dasari <70538475+kmshravani@users.noreply.github.com>
2021-01-04 23:53:49 +0530

    basics-classes-objects.py

---
+++ Day 16/basics-classes-objects.py
@@ -0,0 +1,10 @@
+'''Objects and Classes'''
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
