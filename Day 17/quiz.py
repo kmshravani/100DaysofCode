ef57b8e5be1b728b121f680843fcfdf33947e053
Shravani Dasari <70538475+kmshravani@users.noreply.github.com>
2021-02-28 16:54:02 +0530

    Update quiz.py

--- Day 17/quiz.py
+++ Day 17/quiz.py
@@ -1,1 +1,16 @@
+#Creating Class, Object, Attributes, Methods in a small quiz game project. 
+class Question:
+  def __init__(self,q_text,q_answer):
+    self.text = q_text
+    self.answer = q_answer
 
+new_q = Question("What is?", "True")
+print(new_q.text)
+
+
+
+
+#PascalCase is usually used for Class names in Python
+#snake_case for variables and others in Python
+#attributes are variables associated with an object. Almost just like a new variable but we attach it to an object.
+#Objects are instantiations of Classes.
