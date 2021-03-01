25b916d332d86b1515dfc5f1a509233f326bf4f6
Shravani Dasari <70538475+kmshravani@users.noreply.github.com>
2021-03-01 23:56:31 +0530

    Quiz game

---
+++ Day 17/main.py
@@ -0,0 +1,14 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for i in question_data:
+  question_text = i['text']
+  question_answer = i['answer']
+  new_q = Question(question_text,question_answer)
+  question_bank.append(new_q)
+
+print(question_bank)
+print(question_bank[0].text)
+print(question_bank[0].answer)
